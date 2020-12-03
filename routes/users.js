4e84dbec43c4c8c00d06117d3322e021fd947f5e
FarihulR <farihulrouf@gmail.com>
2020-12-04 00:28:22 +0700

    add API update user

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const usersHandler = require('./handler/users');
+const verifyToken = require('../middlewares/verifyToken');
+
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
+router.put('/',verifyToken, usersHandler.update);
+
+router.get('/',verifyToken, usersHandler.getUser);
 
 module.exports = router;
